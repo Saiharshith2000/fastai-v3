da24ecb9fa3c69bf402b4a69d4ebdf98bd32383f
Saiharshith2000 <61926079+Saiharshith2000@users.noreply.github.com>
2020-03-08 16:27:17 +0530

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -29,7 +29,10 @@ function analyze() {
   xhr.onload = function(e) {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
-      el("result-label").innerHTML = `Result = ${response["result"]}`;
+      
+      var i= `Result = ${response["result"]}`;
+      el("result-label").innerHTML =i;
+      console.log(i);
     }
     el("analyze-button").innerHTML = "Analyze";
   };
